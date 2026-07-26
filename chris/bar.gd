ec45902d882bf05eb8add8b809e97b384cb6f66c
crazyman222214 <schadejr617@gmail.com>
2026-07-26 02:23:22 -0600

    Finalize Basic Bar Stuffs

--- chris/bar.gd
+++ chris/bar.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 var bar_scene = preload("res://chris/bar_menu.tscn") 
 
+@onready var wall : StaticBody2D = $Wall
+@onready var timer: Timer = $Timer
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Chris_Singleton.leave_bar.connect(leave_bar)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,12 +17,16 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("Im barring it rn")
 	var instance = bar_scene.instantiate()
 	add_child(instance)
 	
 
 func leave_bar() -> void:
 	print("woah im leaving")
-	Chris_Singleton.leave_bar.emit()
-	
+	wall.queue_free()
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
+	pass # Replace with function body.
