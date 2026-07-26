45917eb3020787be99c7b1d02f0396987e9c39c0
piper <jazzedkirb@gmail.com>
2026-07-26 11:02:08 -0600

    left and right arrows

--- piper/test_liftoff_bar.gd
+++ piper/test_liftoff_bar.gd
@@ -9,6 +9,8 @@ extends ProgressBar
 @onready var charging_audio: AudioStreamPlayer = $Charging_Audio
 @onready var music: AudioStreamPlayer = %Music
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
+@onready var right: Sprite2D = $"../Right"
+@onready var left: Sprite2D = $"../Left"
 
 var alternating : bool = false
 var initial_press : bool = false
@@ -17,24 +19,39 @@ var input_dir : Vector2
 var is_charging : bool = false
 
 func _ready() -> void:
+	right.hide()
+	left.hide()
 	lightning_timer.start(3.0)
 
 ##left is false right is true
 func _input(event: InputEvent) -> void:
 	if is_charging:
+
 		if !initial_press:
 			if event.is_action_pressed("left"):
+				left_pressed()
 				alternating = false
 				initial_press = true
 			if event.is_action_pressed("right"):
+				right_pressed()
 				alternating = true
 				initial_press = true
 		else:
 			input_dir = Input.get_vector("left", "right", "up", "down")
 			if event.is_action("left") and alternating and input_dir.x < 0:
 				alternate()
+				left_pressed()
 			if event.is_action("right") and !alternating and input_dir.x > 0:
 				alternate()
+				right_pressed()
+
+func right_pressed():
+	right.texture = load("res://sorsha/Extra_Assets/Button_Down.png")
+	left.texture = load("res://sorsha/Extra_Assets/Button_Up.png")
+
+func left_pressed():
+	left.texture = load("res://sorsha/Extra_Assets/Button_Down.png")
+	right.texture = load("res://sorsha/Extra_Assets/Button_Up.png")
 
 func alternate():
 	alternating = !alternating
@@ -51,12 +68,16 @@ func _process(_delta: float) -> void:
 func _on_lightning_timer_timeout() -> void:
 	is_charging = !is_charging
 	if is_charging:
+		left.show()
+		right.show()
 		charging_audio.play()
 		lightning_timer.start(10)
 	else:
 		## signal to stats here that the cannon has started
 		#audio_stream_player.stream = load()
 		launch()
+		right.hide()
+		left.hide()
 		music.play()
 
 func launch():
